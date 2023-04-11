43db7e447cf0196aa6554af4ebd6727f5263b751
hhz329 <70587401+hhz329@users.noreply.github.com>
2023-04-11 22:44:03 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.41 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String new_id) {
         
         new_id = new_id.toLowerCase();
-        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
+        new_id = new_id.replaceAll("[^a-z0-9\\-_.]","");
         new_id = new_id.replaceAll("[.]{2,}",".");
         new_id = new_id.replaceAll("^[.]", "");
         new_id = new_id.replaceAll("[.]$", "");
